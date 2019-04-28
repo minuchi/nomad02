8988124fbc14119ac9224bbf731502b944f27503
kimkime2 <kkimmotti2@gmail.com>
2019-04-28 15:10:31 +0900

    Set up Graql Server to work better with schema.js

--- src/server.js
+++ src/server.js
@@ -1,21 +1,15 @@
 require("dotenv").config();
 import { GraphQLServer } from "graphql-yoga";
+import logger from "morgan";
+import schema from "./schema";
 
 const PORT = process.env.PORT || 4000;
 
-const typeDefs = `
-    type Query{
-        hello:String!
-    }
-`;
-const resolvers = {
-    Query: {
-        hello: () => "Hi"
-    }
-};
+const server = new GraphQLServer({ schema });
 
-const server = new GraphQLServer({ typeDefs, resolvers });
+// middleware
+server.express.use(logger("dev"));
 
 server.start({port: PORT}, () => {
-    console.log(`Server running on port http://localhost:${PORT}`);
+    console.log(`👍 Server running on port http://localhost:${PORT}`);
 });
